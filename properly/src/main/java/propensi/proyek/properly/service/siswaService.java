11d0f9eae81e251716fd461c68c85c8ba4620183
Muhammad Abdurahman Basyah <muhammad.abdurahman@ui.ac.id>
2024-02-18 12:47:53 +0700

    Update 20 files
    - /properly/gradle/wrapper/gradle-wrapper.jar
    - /properly/gradle/wrapper/gradle-wrapper.properties
    - /properly/src/main/resources/static/bootstrap.bundle.min.js
    - /properly/src/main/resources/static/bootstrap.min.css
    - /properly/src/main/resources/templates/fragments/fragment.html
    - /properly/src/main/resources/templates/home.html
    - /properly/src/main/resources/application.properties
    - /properly/src/test/java/propensi/proyek/properly/ProperlyApplicationTests.java
    - /properly/src/main/java/propensi/proyek/properly/controller/PageController.java
    - /properly/src/main/java/propensi/proyek/properly/model/Siswa.java
    - /properly/src/main/java/propensi/proyek/properly/repository/SiswaDb.java
    - /properly/src/main/java/propensi/proyek/properly/service/siswaService.java
    - /properly/src/main/java/propensi/proyek/properly/service/siswaServiceImpl.java
    - /properly/src/main/java/propensi/proyek/properly/ProperlyApplication.java
    - /properly/.gitignore
    - /properly/Dockerfile
    - /properly/build.gradle
    - /properly/gradlew
    - /properly/gradlew.bat
    - /properly/settings.gradle

--- properly/src/main/java/propensi/proyek/properly/service/siswaService.java
+++
@@ -1,9 +0,0 @@
-package propensi.proyek.properly.service;
-
-import propensi.proyek.properly.model.Siswa;
-
-import java.util.List;
-
-public interface siswaService {
-    List<Siswa> getAllSiswa();
-}
